ad439c991a78f17bb2bc71c7bfacf6328e76b5eb
Mauro <mauropolar12@gmail.com>
2022-07-25 14:00:56 -0300

    subida de archivos para el desafio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//crear un programa que sea un temporizador a la hora de hacer ejercicio//
+
+let ingresaUnValor = parseInt(prompt('Elegi un valor para el cronometro (Max 60 seg)'))
+
+for(let i=ingresaUnValor; i>0; i--){
+alert ('Quedan '+i+' segundos sigue adelante!  ')
+
+
+}
+alert ('Gracias por utilizar GymTempo!');
